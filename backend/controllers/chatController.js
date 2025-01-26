b481ecd11d8d986e48d3c022e872f5dc917d2d5e
Aditya201514049 <adityasingha084@gmail.com>
2025-01-26 22:32:44 +0600

    some comments deleted

--- backend/controllers/chatController.js
+++ backend/controllers/chatController.js
@@ -42,33 +42,7 @@ const createChat = async (req, res) => {
   }
 };
 
-// Send a new message to a chat
-/*
-const sendMessage = async (req, res) => {
-  try {
-    const { chatId, content } = req.body;
-    const senderId = req.user._id;
 
-    const chat = await Chat.findById(chatId);
-    if (!chat) {
-      return res.status(404).json({ message: 'Chat not found' });
-    }
-
-    if (![chat.sender.toString(), chat.recipient.toString()].includes(senderId.toString())) {
-      return res.status(403).json({ message: 'Unauthorized to send messages in this chat' });
-    }
-
-    const newMessage = new Message({ sender: senderId, content, chatId });
-    await newMessage.save();
-
-    await Chat.findByIdAndUpdate(chatId, { content }, { new: true });
-    res.status(201).json(newMessage);
-  } catch (error) {
-    console.error(`Error in sendMessage: ${error.message}`);
-    res.status(500).json({ message: 'Error sending message' });
-  }
-};
-*/
 
 // Send a new message to a chat
 
@@ -118,25 +92,7 @@ const getMessages = async (req, res) => {
   }
 };
 
-// Get all chats for the logged-in user
-/*
-const getChats = async (req, res) => {
-  try {
-    const userId = req.user._id;
 
-    const chats = await Chat.find({
-      $or: [{ sender: userId }, { recipient: userId }],
-    })
-      .populate('sender', 'name email')
-      .populate('recipient', 'name email')
-     
-    res.status(200).json(chats);
-  } catch (error) {
-    console.error(`Error in getChats: ${error.message}`);
-    res.status(500).json({ message: 'Error retrieving chats' });
-  }
-};
-*/
 
 const getChats = async (req, res) => {
   try {
@@ -165,35 +121,7 @@ const getChats = async (req, res) => {
 };
 
 
-// Get a specific chat by its ID
-/*
-const getChatById = async (req, res) => {
-  try {
-    const { chatId } = req.params;
-    const userId = req.user._id; // Retrieved from authMiddleware
 
-    const chat = await Chat.findById(chatId)
-      .populate('sender', 'name email')
-      .populate('recipient', 'name email');
-
-    if (!chat) {
-      return res.status(404).json({ message: 'Chat not found' });
-    }
-
-    // Check if the logged-in user is part of the chat
-    if (chat.sender._id.toString() !== userId.toString() && 
-        chat.recipient._id.toString() !== userId.toString() 
-      ) {
-      return res.status(403).json({ message: 'You are not authorized to view this chat' });
-    }
-
-    res.status(200).json(chat);
-  } catch (error) {
-    console.error(`Error in getChatById: ${error.message}`);
-    res.status(500).json({ message: 'Error retrieving chat' });
-  }
-};
-*/
 
 const getChatById = async (req, res) => {
   try {
